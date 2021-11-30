4a3f4c737b76d9fa8492b65fa94701b741673499
Dominik Raczyński <s18806@pjwstk.edu.pl>
2021-11-30 02:52:58 +0100

    test

--- src/main/java/com/skni/workshopspring3/WorkshopSpring3Application.java
+++ src/main/java/com/skni/workshopspring3/WorkshopSpring3Application.java
@@ -2,7 +2,7 @@ package com.skni.workshopspring3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class WorkshopSpring3Application {
 
